6ab4b3d8770fc0e8fa0b4516d6407364110ce6a6
Hack Reactor Student <student@hackreactor.com>
2017-06-12 17:31:40 -0700

    add client code from chatterbox

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+// uses mysql npm model> connects to db server running from cpu
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
